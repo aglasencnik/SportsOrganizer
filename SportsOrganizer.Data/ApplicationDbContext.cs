463fcbe7d81b51f4755caf4cf02409a0090e8b43
Amadej Glasencnik <amadej.glasencnik@gmail.com>
2023-04-11 17:22:35 +0200

    Add ApplicationDbContext, DbContextFactory and interface.

---
+++ SportsOrganizer.Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsOrganizer.Data.Models;
+
+namespace SportsOrganizer.Data;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<TeamModel> Teams { get; set; }
+    public DbSet<UserModel> Users { get; set; }
+    public DbSet<ActivityModel> Activities { get; set; }
+    public DbSet<UserActivityModel> UserActivities { get; set; }
+    public DbSet<ActivityResultModel> ActivityResults { get; set; }
+    public DbSet<PlayerResultModel> PlayerResults { get; set; }
+}
---
+++ SportsOrganizer.Data/IApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsOrganizer.Data.Models;
+
+namespace SportsOrganizer.Data;
+
+public interface IApplicationDbContext
+{
+    public DbSet<TeamModel> Teams { get; set; }
+    public DbSet<UserModel> Users { get; set; }
+    public DbSet<ActivityModel> Activities { get; set; }
+    public DbSet<UserActivityModel> UserActivities { get; set; }
+    public DbSet<ActivityResultModel> ActivityResults { get; set; }
+    public DbSet<PlayerResultModel> PlayerResults { get; set; }
+}
