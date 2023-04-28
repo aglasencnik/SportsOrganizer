be1ef58922383d531adde47c460e153b545cb2da
Amadej Glasencnik <amadej.glasencnik@gmail.com>
2023-04-28 17:58:49 +0200

    Change Admin HomepageEditor page.

--- SportsOrganizer.Server/Pages/Admin/HomepageEditor.razor.cs
+++ SportsOrganizer.Server/Pages/Admin/HomepageEditor.razor.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Blazored.Toast.Services;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.Localization;
 using SportsOrganizer.Data.Enums;
+using SportsOrganizer.Server.Enums;
+using SportsOrganizer.Server.Interfaces;
+using SportsOrganizer.Server.Models;
 using SportsOrganizer.Server.Utils;
 using System.Security.Claims;
 
@@ -21,6 +25,14 @@ public class HomepageEditorBase : ComponentBase
     [Inject]
     protected NavigationManager NavigationManager { get; set; }
 
+    [Inject]
+    protected ILiteDbService<AppSettingsModel> LiteDbService { get; set; }
+
+    [Inject]
+    protected IToastService ToastService { get; set; }
+
+    protected string HomePageHtml { get; set; }
+
     protected override async Task OnInitializedAsync()
     {
         var authState = await AuthState;
@@ -29,5 +41,40 @@ public class HomepageEditorBase : ComponentBase
         if (user.Identities.Count() == 0
             || user.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value != UserType.Admin.ToString())
             NavigationManager.NavigateTo("/");
+
+        var homepageObj = MemoryStorage.GetValue(KeyValueType.Homepage);
+
+        if (homepageObj != null) HomePageHtml = (string)homepageObj;
+        else HomePageHtml = string.Empty;
+    }
+
+    protected void SaveChanges()
+    {
+        try
+        {
+            var homepageObj = LiteDbService.GetAll().FirstOrDefault(x => x.KeyValueType == KeyValueType.Homepage);
+
+            if (homepageObj == null)
+            {
+                LiteDbService.Insert(new AppSettingsModel
+                {
+                    KeyValueType = KeyValueType.Homepage,
+                    Value = HomePageHtml
+                });
+            }
+            else
+            {
+                homepageObj.Value = HomePageHtml;
+
+                LiteDbService.Update(homepageObj);
+            }
+
+            MemoryStorage.SetValue(KeyValueType.Homepage, HomePageHtml);
+            ToastService.ShowSuccess(Localizer["SucccessToast"]);
+        }
+        catch
+        {
+            ToastService.ShowError(Localizer["ErrorToast"]);
+        }
     }
 }
