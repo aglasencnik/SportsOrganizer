ec1a5ca25a0addc2dfe18c72030a4f08f41898d0
Amadej Glasencnik <amadej.glasencnik@gmail.com>
2023-04-28 14:04:47 +0200

    Add authorization and localization to Admin pages.

--- SportsOrganizer.Server/Pages/Admin/UpdateAndVersioning.razor.cs
+++ SportsOrganizer.Server/Pages/Admin/UpdateAndVersioning.razor.cs
@@ -1,8 +1,33 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.Localization;
+using SportsOrganizer.Data.Enums;
+using SportsOrganizer.Server.Utils;
+using System.Security.Claims;
 
 namespace SportsOrganizer.Server.Pages.Admin;
 
 public class UpdateAndVersioningBase : ComponentBase
 {
+    [Inject]
+    public IStringLocalizer<UpdateAndVersioning> Localizer { get; set; }
 
+    [Inject]
+    public MemoryStorageUtility MemoryStorage { get; set; }
+
+    [CascadingParameter]
+    public Task<AuthenticationState> AuthState { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        var authState = await AuthState;
+        var user = authState.User;
+
+        if (user.Identities.Count() == 0
+            || user.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value != UserType.Admin.ToString())
+            NavigationManager.NavigateTo("/");
+    }
 }
