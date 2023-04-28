1896c826ee5802f5aded611621bac683f57ba91e
Amadej Glasencnik <amadej.glasencnik@gmail.com>
2023-04-28 21:06:49 +0200

    Change admin ThemeSelector page, and _Host file.

--- SportsOrganizer.Server/Pages/Admin/ThemeSelector.razor.cs
+++ SportsOrganizer.Server/Pages/Admin/ThemeSelector.razor.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Blazored.Toast.Services;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.Localization;
+using Microsoft.JSInterop;
 using SportsOrganizer.Data.Enums;
+using SportsOrganizer.Server.Enums;
+using SportsOrganizer.Server.Interfaces;
+using SportsOrganizer.Server.Models;
 using SportsOrganizer.Server.Utils;
 using System.Security.Claims;
+using System.Text.RegularExpressions;
 
 namespace SportsOrganizer.Server.Pages.Admin;
 
@@ -21,6 +27,18 @@ public class ThemeSelectorBase : ComponentBase
     [Inject]
     protected NavigationManager NavigationManager { get; set; }
 
+    [Inject]
+    protected ILiteDbService<AppSettingsModel> LiteDbService { get; set; }
+
+    [Inject]
+    protected IToastService ToastService { get; set; }
+
+    [Inject]
+    protected IJSRuntime JSRuntime { get; set; }
+
+    protected string BootswatchUrl { get; set; } = "https://bootswatch.com/";
+    protected string OldTheme { get; set; }
+
     protected override async Task OnInitializedAsync()
     {
         var authState = await AuthState;
@@ -29,5 +47,44 @@ public class ThemeSelectorBase : ComponentBase
         if (user.Identities.Count() == 0
             || user.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value != UserType.Admin.ToString())
             NavigationManager.NavigateTo("/");
+
+        var oldThemeObj = MemoryStorage.GetValue(KeyValueType.Theme);
+
+        if (oldThemeObj != null) OldTheme = (string)oldThemeObj;
+        else OldTheme = string.Empty;
+    }
+
+    protected async Task SelectTheme(string theme)
+    {
+        try
+        {
+            var liteDbResult = LiteDbService.GetAll();
+            var themeObj = liteDbResult.FirstOrDefault(x => x.KeyValueType == KeyValueType.Theme);
+
+            if (themeObj == null)
+            {
+                LiteDbService.Insert(new AppSettingsModel
+                {
+                    KeyValueType = KeyValueType.Theme,
+                    Value = theme
+                });
+            }
+            else
+            {
+                themeObj.Value = theme;
+
+                LiteDbService.Update(themeObj);
+            }
+
+            MemoryStorage.SetValue(KeyValueType.Theme, theme);
+            await JSRuntime.InvokeVoidAsync("changeTheme", theme);
+            OldTheme = theme;
+            StateHasChanged();
+            ToastService.ShowSuccess(Localizer["SuccessToast"]);
+        }
+        catch
+        {
+            ToastService.ShowError(Localizer["ErrorToast"]);
+        }
     }
 }
