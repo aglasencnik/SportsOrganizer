2f966936d9222d877d936bc380ce73b4f4621e56
Amadej Glasencnik <amadej.glasencnik@gmail.com>
2023-04-29 19:36:27 +0200

    Change User Management page and resolve a bug in Activity modal.

--- SportsOrganizer.Server/Components/AdminComponents/AdminUserEditModalComponent/AdminUserEditModal.razor.cs
+++ SportsOrganizer.Server/Components/AdminComponents/AdminUserEditModalComponent/AdminUserEditModal.razor.cs
@@ -1,8 +1,155 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Blazored.Toast.Services;
+using Blazorise;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using SportsOrganizer.Data.Models;
+using SportsOrganizer.Data;
+using SportsOrganizer.Server.Enums;
+using SportsOrganizer.Server.Models;
+using SportsOrganizer.Server.Services;
+using System.Diagnostics;
+using SportsOrganizer.Data.Enums;
 
 namespace SportsOrganizer.Server.Components.AdminComponents.AdminUserEditModalComponent;
 
 public class AdminUserEditModalBase : ComponentBase
 {
+    [Parameter]
+    public ModalParametersModel ModalParameters { get; set; }
 
+    [Inject]
+    protected IStringLocalizer<AdminUserEditModal> Localizer { get; set; }
+
+    [Inject]
+    protected IToastService Toast { get; set; }
+
+    [Inject]
+    protected IModalService ModalService { get; set; }
+
+    [Inject]
+    protected ApplicationDbContextService DbContextService { get; set; }
+
+    private ApplicationDbContext DbContext => DbContextService.GetDbContext();
+
+    protected UserModel User { get; set; } = new();
+    protected List<UserActivityModel> UserActivities { get; set; } = new();
+    protected List<ActivityModel> Activities { get; set; } = new();
+    protected IReadOnlyList<int> SelectedActivityIds { get; set; }
+
+    protected override void OnInitialized()
+    {
+        if (ModalParameters.EditType != EditType.Add)
+        {
+            User = DbContext.Users.FirstOrDefault(x => x.Id == ModalParameters.Id);
+            UserActivities = DbContext.UserActivities.Where(x => x.UserId == User.Id).ToList();
+            SelectedActivityIds = UserActivities.Select(x => x.ActivityId).ToList();
+        }
+
+        User.UserType = UserType.User;
+        Activities = DbContext.Activities.ToList();
+    }
+
+    protected async Task Confirm()
+    {
+        try
+        {
+            if (!string.IsNullOrWhiteSpace(User.Username) &&
+                !string.IsNullOrWhiteSpace(User.Password) &&
+                User.UserType != UserType.Guest)
+            {
+                if (ModalParameters.EditType == EditType.Add)
+                {
+                    await DbContext.Users.AddAsync(User);
+                    await DbContext.SaveChangesAsync();
+
+                    if (SelectedActivityIds != null && SelectedActivityIds.Count != 0)
+                    {
+                        foreach (var activityId in SelectedActivityIds)
+                        {
+                            UserActivities.Add(new UserActivityModel { ActivityId = activityId, UserId = User.Id });
+                        }
+
+                        await DbContext.UserActivities.AddRangeAsync(UserActivities);
+                    }
+
+                    await DbContext.SaveChangesAsync();
+
+                    Toast.ShowSuccess(Localizer["SuccessToast"]);
+                    await ModalService.Hide();
+                }
+                else if (ModalParameters.EditType == EditType.Edit)
+                {
+                    DbContext.Users.Update(User);
+
+                    var userActivityIds = UserActivities.Select(x => x.ActivityId).ToList();
+
+                    if (SelectedActivityIds != null && SelectedActivityIds.Count != 0)
+                    {
+                        if (UserActivities != null && UserActivities.Count != 0)
+                        {
+                            var deletedUserActivityIds = userActivityIds.Except(SelectedActivityIds).ToList();
+                            var deletedUserActivities = UserActivities.Where(x => deletedUserActivityIds.Contains(x.ActivityId)).ToList();
+                            DbContext.UserActivities.RemoveRange(deletedUserActivities);
+
+                            var newUserActivityIds = SelectedActivityIds.Except(userActivityIds).ToList();
+                            var newUserActivities = new List<UserActivityModel>();
+
+                            foreach (var activityId in newUserActivityIds)
+                            {
+                                newUserActivities.Add(new UserActivityModel { ActivityId = activityId, UserId = User.Id });
+                            }
+
+                            await DbContext.UserActivities.AddRangeAsync(newUserActivities);
+                        }
+                        else
+                        {
+                            var newUserActivities = new List<UserActivityModel>();
+
+                            foreach (var activityId in SelectedActivityIds)
+                            {
+                                newUserActivities.Add(new UserActivityModel { ActivityId = activityId, UserId = User.Id });
+                            }
+
+                            await DbContext.UserActivities.AddRangeAsync(newUserActivities);
+                        }
+                    }
+                    else if (UserActivities != null && UserActivities.Count != 0)
+                    {
+                        DbContext.UserActivities.RemoveRange(UserActivities);
+                    }
+
+                    await DbContext.SaveChangesAsync();
+
+                    Toast.ShowSuccess(Localizer["SuccessToast"]);
+                    await ModalService.Hide();
+                }
+                else if (ModalParameters.EditType == EditType.Delete)
+                {
+                    DbContext.Users.Remove(User);
+                    if (UserActivities != null && UserActivities.Count != 0)
+                        DbContext.UserActivities.RemoveRange(UserActivities);
+
+                    await DbContext.SaveChangesAsync();
+
+                    Toast.ShowSuccess(Localizer["SuccessToast"]);
+
+                    await ModalService.Hide();
+                }
+            }
+            else
+            {
+                if (ModalParameters.EditType == EditType.Edit)
+                {
+                    DbContext.Entry(User).State = EntityState.Detached;
+                }
+                Toast.ShowWarning(Localizer["WarningToast"]);
+            }
+        }
+        catch
+        {
+            Toast.ShowError(Localizer["ErrorToast"]);
+            await ModalService.Hide();
+        }
+    }
 }
--- SportsOrganizer.Server/Pages/Admin/UserManagement.razor.cs
+++ SportsOrganizer.Server/Pages/Admin/UserManagement.razor.cs
@@ -1,7 +1,15 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Blazored.Toast.Services;
+using Blazorise;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.Localization;
+using SportsOrganizer.Data;
 using SportsOrganizer.Data.Enums;
+using SportsOrganizer.Data.Models;
+using SportsOrganizer.Server.Components.AdminComponents.AdminUserEditModalComponent;
+using SportsOrganizer.Server.Enums;
+using SportsOrganizer.Server.Models;
+using SportsOrganizer.Server.Services;
 using SportsOrganizer.Server.Utils;
 using System.Security.Claims;
 
@@ -21,6 +29,25 @@ public class UserManagementBase : ComponentBase
     [Inject]
     protected NavigationManager NavigationManager { get; set; }
 
+    [Inject]
+    protected IToastService ToastService { get; set; }
+
+    [Inject]
+    protected IModalService ModalService { get; set; }
+
+    [Inject]
+    protected IMessageService MessageService { get; set; }
+
+    [Inject]
+    protected ApplicationDbContextService DbContextService { get; set; }
+
+    private ApplicationDbContext DbContext => DbContextService.GetDbContext();
+
+    public List<UserModel> Users { get; set; }
+    protected List<UserActivityModel> UserActivities { get; set; }
+    protected List<ActivityModel> Activities { get; set; }
+    protected ThemeContrast ThemeContrast { get; set; }
+
     protected override async Task OnInitializedAsync()
     {
         var authState = await AuthState;
@@ -29,5 +56,87 @@ public class UserManagementBase : ComponentBase
         if (user.Identities.Count() == 0
             || user.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value != UserType.Admin.ToString())
             NavigationManager.NavigateTo("/");
+
+        Users = DbContext.Users.ToList();
+        UserActivities = DbContext.UserActivities.ToList();
+        var activityIds = UserActivities.Select(x => x.ActivityId).ToList();
+        Activities = DbContext.Activities.Where(x => activityIds.Contains(x.Id)).ToList();
+
+        var themeObj = MemoryStorage.GetValue(KeyValueType.DataGridThemeContrast);
+
+        if (themeObj != null) ThemeContrast = (ThemeContrast)themeObj;
+        else ThemeContrast = ThemeContrast.Light;
+    }
+
+    protected void OpenAddModal()
+    {
+        var modalParameters = new ModalParametersModel
+        {
+            EditType = EditType.Add
+        };
+
+        ModalService.Show<AdminUserEditModal>(x => x.Add(x => x.ModalParameters, modalParameters),
+            new ModalInstanceOptions { Closed = new EventCallback(this, OnModalClosed), UseModalStructure = false });
+    }
+
+    protected void OpenEditModal(int id)
+    {
+        var modalParameters = new ModalParametersModel
+        {
+            EditType = EditType.Edit,
+            Id = id
+        };
+
+        ModalService.Show<AdminUserEditModal>(x => x.Add(x => x.ModalParameters, modalParameters),
+            new ModalInstanceOptions { Closed = new EventCallback(this, OnModalClosed), UseModalStructure = false });
+    }
+
+    protected void OpenDeleteModal(int id)
+    {
+        var modalParameters = new ModalParametersModel
+        {
+            EditType = EditType.Delete,
+            Id = id
+        };
+
+        ModalService.Show<AdminUserEditModal>(x => x.Add(x => x.ModalParameters, modalParameters),
+            new ModalInstanceOptions { Closed = new EventCallback(this, OnModalClosed), UseModalStructure = false });
+    }
+
+    protected async Task DeleteAll()
+    {
+        if (await MessageService.Confirm(Localizer["ConfModalContent"], Localizer["ConfModalHeader"]))
+        {
+            var firstAdmin = Users.FirstOrDefault(x => x.UserType == UserType.Admin);
+
+            var usersToDelete = Users.Where(x => x.Id != firstAdmin.Id).ToList();
+            var userIds = usersToDelete.Select(x => x.Id);
+            var userActivitiesToDelete = UserActivities.Where(x => userIds.Contains(x.UserId)).ToList();
+
+            DbContext.UserActivities.RemoveRange(userActivitiesToDelete);
+            DbContext.Users.RemoveRange(usersToDelete);
+            await DbContext.SaveChangesAsync();
+            ToastService.ShowSuccess(Localizer["SuccessToast"]);
+            OnModalClosed();
+        }
+    }
+
+    protected void Export(ExportFileType fileType)
+    {
+
+    }
+
+    protected void Print()
+    {
+
+    }
+
+    private void OnModalClosed()
+    {
+        Users = DbContext.Users.ToList();
+        UserActivities = DbContext.UserActivities.ToList();
+        var activityIds = UserActivities.Select(x => x.ActivityId).ToList();
+        Activities = DbContext.Activities.Where(x => activityIds.Contains(x.Id)).ToList();
+        StateHasChanged();
     }
 }
